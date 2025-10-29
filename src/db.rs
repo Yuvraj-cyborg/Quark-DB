13f5d6f700950a645398bd27703a8e85b87419d8
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-10-29 23:04:56 +0530

    feat: first version of db

---
+++ src/db.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+
+struct CacheDB {
+    store: HashMap<String, String>,
+}
+
+impl CacheDB {
+    pub fn new() {
+        CacheDB {
+            store: HashMap::new();
+        }
+    }
+}
+
+// inserting data into the cache
+pub fn put(&mut self, key: String, value: String) {
+    self.store.insert(key,value);
+}
+
+// fetching data from the cache using its key
+pub fn get(&self, key: &str) -> Option<&String> {
+    self.store.get(key);
+}
+
+// deleting/removing data key from table and retruns true/false accordingly
+pub fn delete(&mut self, key: &str) -> bool {
+    self.store.remove(key).is_some();
+}
+
+// how many entries are there in cache
+pub fn size(&self) -> usize {
+    self.store.len();
+}
+
+
