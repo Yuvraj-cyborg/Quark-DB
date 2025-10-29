4ed75dda445f9f2a4e8af2101fd2b55ae9d094d6
Yuvraj <yuvrajbiswalofficial@gmail.com>
2025-10-30 00:01:00 +0530

    feat: added features and implemented cli using clap

--- src/db.rs
+++ src/db.rs
@@ -1,35 +1,36 @@
 use std::collections::HashMap;
 
-struct CacheDB {
+pub struct CacheDB {
     store: HashMap<String, String>,
 }
 
 impl CacheDB {
-    pub fn new() {
+    pub fn new() -> Self {
         CacheDB {
-            store: HashMap::new();
+            store: HashMap::new(),
         }
     }
-}
 
-// inserting data into the cache
-pub fn put(&mut self, key: String, value: String) {
-    self.store.insert(key,value);
-}
+    // inserting data into the cache
+    pub fn put(&mut self, key: String, value: String) {
+        self.store.insert(key,value);
+    }
 
-// fetching data from the cache using its key
-pub fn get(&self, key: &str) -> Option<&String> {
-    self.store.get(key);
-}
+    // fetching data from the cache using its key
+    pub fn get(&self, key: &str) -> Option<&String> {
+        self.store.get(key)
+    }
 
-// deleting/removing data key from table and retruns true/false accordingly
-pub fn delete(&mut self, key: &str) -> bool {
-    self.store.remove(key).is_some();
-}
+    // deleting/removing data key from table and retruns true/false accordingly
+    pub fn delete(&mut self, key: &str) -> bool {
+        self.store.remove(key).is_some()
+    }
 
-// how many entries are there in cache
-pub fn size(&self) -> usize {
-    self.store.len();
+    // how many entries are there in cache
+    pub fn size(&self) -> usize {
+        self.store.len()
+    }
+
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
-fn main() {
-    println!("Hello, world!");
+use Quark_db::db::CacheDB;
+use clap::{Parser,Subcommand};
+
+#[derive(Parser, Debug)]
+#[command(name = "Quark-DB", version = "0.1.0", author = "Yuvraj Biswal")]
+#[command(
+    about = "A mini cache DB",
+    long_about = "A cache DB (just a hashmap as a db) in Rust"
+)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 }
+
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Put {
+      key: String,
+      value: String,
+    },
+    Get {
+      key: String,
+    },
+    Delete {
+      key: String,
+    },
+    Size,
+}
+
+fn main() {
+    let mut db = CacheDB::new();
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::Put { key, value } => {
+            db.put(key.clone(), value.clone());
+            println!("Inserted ('{key}', '{value}')");
+        }
+
+        Commands::Get { key } => match db.get(&key) {
+            Some(value) => println!("{key} => {value}"),
+            None => println!("Key '{key}' not found"),
+        },
+
+        Commands::Delete { key } => {
+            if db.delete(&key) {
+                println!("Deleted key '{key}'");
+            } else {
+                println!("Key '{key}' not found");
+            }
+        }
+
+        Commands::Size => {
+            println!("DB size: {}", db.size());
+        }
+    }
+
+}
+
+
+
+
+
